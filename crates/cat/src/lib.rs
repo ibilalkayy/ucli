bdb42d1768ee085a1650cb9ff414060e45490125
Bilal Khan <bilalkhanrecovered@gmail.com>
2025-07-25 00:49:07 +0500

    added the tests for cat command of all the two flags

--- crates/cat/src/lib.rs
+++ crates/cat/src/lib.rs
@@ -7,23 +7,71 @@ pub mod cat {
     }
 
     impl CatData {
-        pub fn cat_options(&self) {
+        pub fn cat_output(&self) -> String {
             match &self.path {
                 Some(file) => {
+                    let content =
+                        fs::read_to_string(file).expect("Err: failed to read the file content");
                     if self.number {
-                        let content =
-                            fs::read_to_string(file).expect("Err: failed to read the file content");
-                        for (index, line) in content.lines().enumerate() {
-                            println!("{} {}", index + 1, line);
-                        }
+                        content
+                            .lines()
+                            .enumerate()
+                            .map(|(x, line)| format!("{} {}", x + 1, line))
+                            .collect::<Vec<_>>()
+                            .join("\n")
                     } else {
-                        let content =
-                            fs::read_to_string(file).expect("Err: failed to read the file content");
-                        println!("{}", content);
+                        content
                     }
                 }
-                None => println!("Err: file has not been found"),
+                None => "Err: file has not been found".to_string(),
             }
         }
+
+        pub fn cat_options(&self) {
+            let output = self.cat_output();
+            println!("{}", output);
+        }
+    }
+}
+
+#[cfg(test)]
+mod cat_tests {
+    use crate::cat::CatData;
+    use std::fs::File;
+    use std::io::Write;
+    use std::path::PathBuf;
+
+    #[test]
+    fn test_read_file_num_false() {
+        let file_path = PathBuf::from("test_file.txt");
+
+        let mut file = File::create(&file_path).expect("Err: failed to create a file");
+        file.write_all("Line 1\nLine 2".as_bytes())
+            .expect("Err: failed to write to test file");
+
+        let cat_data = CatData {
+            path: Some(file_path.to_str().unwrap().to_string()),
+            number: false,
+        };
+
+        let output = cat_data.cat_output();
+        assert_eq!(output, "Line 1\nLine 2")
+    }
+
+    #[test]
+    fn test_read_file_num_true() {
+        let file_path = PathBuf::from("test_file.txt");
+
+        let mut file = File::create(&file_path).expect("Err: failed to create a file");
+        file.write_all("Line 1\nLine 2".as_bytes())
+            .expect("Err: failed to write to test file");
+
+        let cat_data = CatData {
+            path: Some(file_path.to_str().unwrap().to_string()),
+            number: true,
+        };
+
+        let output = cat_data.cat_output();
+        assert_eq!(output, "1 Line 1\n2 Line 2")
     }
 }
