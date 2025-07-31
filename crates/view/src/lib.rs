fb926e68e4147b79001f1c674251923d97356f4a
Bilal Khan <bilalkhanrecovered@gmail.com>
2025-08-01 01:14:36 +0500

    wrote the tests for view command to read the file data

--- crates/view/src/lib.rs
+++ crates/view/src/lib.rs
@@ -23,3 +23,35 @@ pub mod view {
         }
     }
 }
+
+#[cfg(test)]
+mod view_tests {
+    use super::view::ViewData;
+    use std::{
+        fs::{self, File},
+        io::Write,
+        path::PathBuf,
+    };
+
+    #[test]
+    fn test_read_file() {
+        let file_name = PathBuf::from("test.txt");
+        let content = "Line1\nLine2\nLine3";
+
+        let mut file = File::create(&file_name).expect("Err: failed to create a file");
+        file.write_all(content.as_bytes())
+            .expect("Err: failed to write to a file");
+
+        let view = ViewData {
+            file: Some(file_name.to_str().unwrap().to_string()),
+            lines: 2,
+        };
+
+        view.view_options();
+
+        let read_content = fs::read_to_string(&file_name).expect("Failed to read file");
+        assert_eq!(read_content, content);
+
+        fs::remove_file(file_name).expect("Err: failed to remove the file");
+    }
+}
