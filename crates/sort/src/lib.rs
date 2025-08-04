38277664a0f903974ddcbce14ad30e94e131e0ba
Bilal Khan <bilalkhanrecovered@gmail.com>
2025-08-04 12:00:44 +0500

    removed the duplicate function

--- crates/sort/src/lib.rs
+++ crates/sort/src/lib.rs
@@ -118,39 +118,6 @@ mod sort_tests {
     }
 
     #[test]
-    fn test_sort_trimming() {
-        let file_name = PathBuf::from("test3.txt");
-        let content = "1 Line\n\n2 Line\n3 Line\n\n4 Line";
-
-        let mut file = File::create(&file_name).expect("Err: failed to create a file");
-        file.write_all(content.as_bytes())
-            .expect("Err: failed to write to a file");
-
-        let data = SortData {
-            file: Some(file_name.to_str().unwrap().to_string()),
-            reverse: false,
-            number: false,
-        };
-
-        data.sort_options();
-
-        let read_content = fs::read_to_string(&file_name).expect("Err: failed to read the file");
-        let lines: Vec<&str> = read_content
-            .lines()
-            .filter(|line| !line.trim().is_empty())
-            .collect();
-
-        let expected_lines: Vec<&str> = content
-            .lines()
-            .filter(|line| !line.trim().is_empty())
-            .collect();
-
-        assert_eq!(lines, expected_lines);
-
-        fs::remove_file(file_name).expect("Err: failed to remove the file");
-    }
-
-    #[test]
     fn test_sort_by_number() {
         let file_name = PathBuf::from("test4.txt");
         let content = "1 Line\n\n2 Line\n3 Line\n\n4 Line";
