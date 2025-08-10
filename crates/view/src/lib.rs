48bc51bf0e4f45021e8b894c61919b41c385d4f1
Bilal Khan <bilalkhanrecovered@gmail.com>
2025-08-10 14:41:48 +0500

    added the tests in separate files and included the pathbuf

--- crates/view/src/lib.rs
+++ crates/view/src/lib.rs
@@ -1,57 +1,23 @@
-pub mod view {
-    use std::fs;
+use std::{fs, path::PathBuf};
 
-    pub struct ViewData {
-        pub file: Option<String>,
-        pub lines: usize,
-    }
-
-    impl ViewData {
-        pub fn view_options(&self) {
-            match &self.file {
-                Some(file) => match fs::read_to_string(file) {
-                    Ok(content) => {
-                        let lines = content.lines().take(self.lines);
-                        for line in lines {
-                            println!("{}", line);
-                        }
-                    }
-                    Err(error) => eprintln!("Err: {}", error),
-                },
-                None => eprintln!("Err: file has not been selected"),
-            }
-        }
-    }
+pub struct ViewData {
+    pub file: Option<PathBuf>,
+    pub lines: usize,
 }
 
-#[cfg(test)]
-mod view_tests {
-    use super::view::ViewData;
-    use std::{
-        fs::{self, File},
-        io::Write,
-        path::PathBuf,
-    };
-
-    #[test]
-    fn test_read_file() {
-        let file_name = PathBuf::from("test.txt");
-        let content = "Line1\nLine2\nLine3";
-
-        let mut file = File::create(&file_name).expect("Err: failed to create a file");
-        file.write_all(content.as_bytes())
-            .expect("Err: failed to write to a file");
-
-        let view = ViewData {
-            file: Some(file_name.to_str().unwrap().to_string()),
-            lines: 2,
-        };
-
-        view.view_options();
-
-        let read_content = fs::read_to_string(&file_name).expect("Err: failed to read the file");
-        assert_eq!(read_content, content);
-
-        fs::remove_file(file_name).expect("Err: failed to remove the file");
+impl ViewData {
+    pub fn view_options(&self) {
+        match &self.file {
+            Some(file) => match fs::read_to_string(file) {
+                Ok(content) => {
+                    let lines = content.lines().take(self.lines);
+                    for line in lines {
+                        println!("{}", line);
+                    }
+                }
+                Err(error) => eprintln!("Err: {}", error),
+            },
+            None => eprintln!("Err: file has not been selected"),
+        }
     }
 }
