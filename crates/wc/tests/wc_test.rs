3a7c27b659d2d4e65b531b4523ce2ab1eefc480f
Bilal Khan <bilalkhanrecovered@gmail.com>
2025-08-13 14:17:20 +0500

    added the tempfile to wc

--- crates/wc/tests/wc_test.rs
+++ crates/wc/tests/wc_test.rs
@@ -1,73 +1,53 @@
-use std::{
-    fs::{self, File},
-    io::Write,
-    path::PathBuf,
-};
+use std::{fs, io::Write};
 
+use tempfile::NamedTempFile;
 use ywc::WcData;
 
 #[test]
 fn test_read_file() {
-    let file_name = PathBuf::from("test1.txt");
     let content = "1 Line\n2 Line\n3 Line";
-
-    let mut file = File::create(&file_name).expect("Err: failed to create a file");
-    file.write_all(content.as_bytes())
-        .expect("Err: failed to write to a file");
+    let mut file = NamedTempFile::new().expect("Err: failed to create a file");
+    write!(file, "{}", content).expect("Err: failed to write to a file");
 
     let data = WcData {
-        file: Some(file_name.to_path_buf()),
+        file: Some(file.path().to_path_buf()),
         lines: false,
         words: false,
         bytes: false,
     };
 
     data.wc_options();
-
-    let read_content = fs::read_to_string(&file_name).expect("Err: failed to read the file");
-
+    let read_content = fs::read_to_string(&file).expect("Err: failed to read the file");
     assert_eq!(read_content, content);
-
-    fs::remove_file(file_name).expect("Err: failed to remove the file");
 }
 
 #[test]
 fn test_count_lines() {
-    let file_name = PathBuf::from("test2.txt");
     let content = "1 Line\n2 Line\n3 Line";
-
-    let mut file = File::create(&file_name).expect("Err: failed to create a file");
-    file.write_all(content.as_bytes())
-        .expect("Err: failed to write to a file");
+    let mut file = NamedTempFile::new().expect("Err: failed to create a file");
+    write!(file, "{}", content).expect("Err: failed to write to a file");
 
     let data = WcData {
-        file: Some(file_name.to_path_buf()),
+        file: Some(file.path().to_path_buf()),
         lines: true,
         words: false,
         bytes: false,
     };
 
     data.wc_options();
-
-    let read_content = fs::read_to_string(&file_name).expect("Err: failed to read the file");
+    let read_content = fs::read_to_string(&file).expect("Err: failed to read the file");
     let lines = read_content.lines().count();
-
     assert_eq!(lines, 3);
-
-    fs::remove_file(file_name).expect("Err: failed to remove the file");
 }
 
 #[test]
 fn test_count_words() {
-    let file_name = PathBuf::from("test3.txt");
     let content = "Line1 Line2 Line3";
-
-    let mut file = File::create(&file_name).expect("Err: failed to create a file");
-    file.write_all(content.as_bytes())
-        .expect("Err: failed to write to a file");
+    let mut file = NamedTempFile::new().expect("Err: failed to create a file");
+    write!(file, "{}", content).expect("Err: failed to write to a file");
 
     let data = WcData {
-        file: Some(file_name.to_path_buf()),
+        file: Some(file.path().to_path_buf()),
         lines: false,
         words: true,
         bytes: false,
@@ -75,7 +55,7 @@ fn test_count_words() {
 
     data.wc_options();
 
-    let read_content = fs::read_to_string(&file_name).expect("Err: failed to read the file");
+    let read_content = fs::read_to_string(&file).expect("Err: failed to read the file");
     let words = read_content.split(' ').enumerate().last();
     let mut result: usize = 0;
     match words {
@@ -84,34 +64,24 @@ fn test_count_words() {
         }
         None => println!("Err: no value found"),
     }
-
     assert_eq!(result, 3);
-
-    fs::remove_file(file_name).expect("Err: failed to remove the file");
 }
 
 #[test]
 fn test_count_bytes() {
-    let file_name = PathBuf::from("test4.txt");
     let content = "1 Line";
-
-    let mut file = File::create(&file_name).expect("Err: failed to create a file");
-    file.write_all(content.as_bytes())
-        .expect("Err: failed to write to a file");
+    let mut file = NamedTempFile::new().expect("Err: failed to create a file");
+    write!(file, "{}", content).expect("Err: failed to write to a file");
 
     let data = WcData {
-        file: Some(file_name.to_path_buf()),
+        file: Some(file.path().to_path_buf()),
         lines: false,
         words: false,
         bytes: true,
     };
 
     data.wc_options();
-
-    let read_content = fs::read_to_string(&file_name).expect("Err: failed to read the file");
+    let read_content = fs::read_to_string(&file).expect("Err: failed to read the file");
     let length = read_content.len();
-
     assert_eq!(length, 6);
-
-    fs::remove_file(file_name).expect("Err: failed to remove the file");
 }
